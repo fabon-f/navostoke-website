b03e581bfb8285530c79ef76056cbec91ad994f7
fabon <syobon.hinata.public@gmail.com>
2022-03-12 02:56:47 +0900

    Add C100 info

---
+++ src/events/c100.tsx
@@ -0,0 +1,27 @@
+import React from "https://deno.land/x/react_deno@17.0.2/react.ts";
+import { ExternalLink, WebpImage } from "../../lib/components.tsx";
+import { getEventData } from "../../lib/helpers.ts";
+import { Data } from "../types.ts";
+
+export const title = "コミックマーケット100";
+export const description = "C100のイベント参加情報";
+
+function imageElement(opt: {type: string; src: string}) {
+    const imgPath= `/img/events/${opt.src}`
+    if (opt.type === "webp") {
+        return <WebpImage {...opt} src={imgPath} />
+    } else {
+        return <img {...opt} src={imgPath} />
+    }
+}
+
+export default (data: Data) => {
+    const eventData = getEventData(data);
+    if (!eventData) { throw new Error("event not found"); }
+    return <main role="main">
+        <h1>コミックマーケット100{ eventData.status && ` (${eventData.status})` }</h1>
+        {eventData.space && <p>スペース: <strong>{eventData.space}</strong></p>}
+        <p>{eventData.url && <ExternalLink href={eventData.url}>イベント公式サイト</ExternalLink>}</p>
+        {imageElement(eventData.img)}
+    </main>;
+}
