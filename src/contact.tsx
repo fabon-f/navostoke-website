29de99473e6a821e380b4a4f4724f4d46ade3d75
fabon <syobon.hinata.public@gmail.com>
2022-06-23 07:00:00 +0900

    Update contact links

--- src/contact.tsx
+++ src/contact.tsx
@@ -13,5 +13,6 @@ export default (data: Data) =>
             <li><ExternalLink href="https://www.fabon.info">ふぁぼんのホームページ</ExternalLink></li>
             <li><ExternalLink href="https://twitter.com/syobon_hinata">ふぁぼんのTwitterアカウント(DMを開放しています)</ExternalLink></li>
             <li><ExternalLink href="https://www.pixiv.net/users/10536791">ふぁぼんのpixivアカウント(主に二次創作小説)</ExternalLink></li>
+            <li><ExternalLink href="https://docs.google.com/forms/d/e/1FAIpQLSdyXOd-p2bS1mVYev7HQ4kibnnHJQBH2rsR-TRO4faCcsfwdw/viewform">読了報告(+感想)フォーム</ExternalLink></li>
         </ul>
     </main>;
