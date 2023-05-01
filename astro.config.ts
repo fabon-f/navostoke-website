ef0b9f6c8e176b0705f50bc41bf89d7560164816
fabon <syobon.hinata.public@gmail.com>
2023-05-02 06:33:21 +0900

    Compress HTML in production build

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [sitemap(), compress({
+    css: false, html: true, img: false, js: false, svg: false, logger: 0
+  })],
   site: "https://navostoke.fabon.info"
 });
