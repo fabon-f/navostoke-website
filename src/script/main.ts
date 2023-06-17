2f597fff169965e672b78b97601f55a2c4b0a1ed
fabon <syobon.hinata.public@gmail.com>
2023-06-18 02:44:34 +0900

    Remove unnecessary DOMContentLoaded event handler

--- src/script/main.ts
+++ src/script/main.ts
@@ -3,38 +3,30 @@ import 'luminous-lightbox/dist/luminous-basic.css'
 
 const webpSupport = document.createElement("canvas").toDataURL("image/webp").indexOf("data:image/webp") == 0
 
-const main = () => {
-  const table: Record<string, HTMLElement[]> = {};
-  for (const elem of document.querySelectorAll("a[data-gallery]")) {
-    if (elem instanceof HTMLAnchorElement) {
-      if (!elem.dataset.gallery) { continue; }
+const table: Record<string, HTMLElement[]> = {};
+for (const elem of document.querySelectorAll("a[data-gallery]")) {
+  if (elem instanceof HTMLAnchorElement) {
+    if (!elem.dataset.gallery) { continue; }
 
-      if (webpSupport && typeof elem.dataset.galleryWebp === 'string') {
-        elem.href = elem.dataset.galleryWebp.endsWith(".webp") ? elem.dataset.galleryWebp : elem.href.replace(/\.[^\.]*$/, ".webp")
-      }
-
-      if (!Array.isArray(table[elem.dataset.gallery])) {
-        table[elem.dataset.gallery] = [];
-      }
-      table[elem.dataset.gallery]!.push(elem);
+    if (webpSupport && typeof elem.dataset.galleryWebp === 'string') {
+      elem.href = elem.dataset.galleryWebp.endsWith(".webp") ? elem.dataset.galleryWebp : elem.href.replace(/\.[^\.]*$/, ".webp")
     }
-  }
 
-  for (const [_key, elems] of Object.entries(table)) {
-    elems.length == 1 ? new Luminous(elems[0]!) : new LuminousGallery(elems);
+    if (!Array.isArray(table[elem.dataset.gallery])) {
+      table[elem.dataset.gallery] = [];
+    }
+    table[elem.dataset.gallery]!.push(elem);
+  }
 }
 
-  const readOnlyTextareas = Array.from(document.querySelectorAll('textarea[readonly]'));
-  for (const textarea of readOnlyTextareas) {
-    if (!(textarea instanceof HTMLTextAreaElement)) { throw new Error(); }
-    textarea.addEventListener('click', () => {
-      textarea.select();
-    });
-  }
-};
+for (const [_key, elems] of Object.entries(table)) {
+  elems.length == 1 ? new Luminous(elems[0]!) : new LuminousGallery(elems);
+}
 
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', () => main());
-} else {
-  main();
+const readOnlyTextareas = Array.from(document.querySelectorAll('textarea[readonly]'));
+for (const textarea of readOnlyTextareas) {
+  if (!(textarea instanceof HTMLTextAreaElement)) { throw new Error(); }
+  textarea.addEventListener('click', () => {
+    textarea.select();
+  });
 }
