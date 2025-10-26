caa4e7cecac02d2eb9ddc4ed07061547d82da14c
fabon <syobon.hinata.public@gmail.com>
2025-10-27 02:58:18 +0900

    Remove `astro-compress` package which is no longer unnecessary

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import preact from '@astrojs/preact';
-import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), sitemap(), compress({
-    CSS: false, HTML: true, Image: false, JavaScript: false, SVG: false, Logger: 0
-  })],
+  integrations: [preact(), sitemap()],
   site: "https://navostoke.fabon.info"
 });
